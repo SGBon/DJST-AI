c70d2c7eb5e3ffcc8222ab2d51d1f47b2c75ec60
sbonada <santiago.bonada@uoit.net>
2017-03-09 15:00:02 -0500

    adding value variable to card

--- src/card.py
+++ src/card.py
@@ -2,7 +2,7 @@
 
 import random
 
-cardValues = ["ace",
+cardNumbers = ["ace",
     "two",
     "three",
     "four",
@@ -18,14 +18,17 @@ cardValues = ["ace",
 
 cardSuits = ["spades","hearts","diamonds","clubs"]
 
+blackjackValues = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+
 # represents a card in the blackjack game
 class Card:
-    def __init__(self,value,suit):
-        self.value = value
+    def __init__(self,number,suit,value):
+        self.number = number
         self.suit = suit
+        self.value = value
 
     def __str__(self):
-        return "<%s:%s>" % (self.value,self.suit)
+        return "<%s:%s:%d>" % (self.number,self.suit,self.value)
 
 # a deck of cards that can be drawn from and shuffled
 class Deck:
@@ -43,8 +46,8 @@ class Deck:
     def reset(self):
         self.cards = []
         for suit in cardSuits:
-            for value in cardValues:
-                self.cards.append(Card(value,suit))
+            for i in range(len(cardNumbers)):
+                self.cards.append(Card(cardNumbers[i],suit,blackjackValues[i]))
 
     # shuffle the deck
     def shuffle(self):
