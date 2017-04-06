aa44016b83a757a0cea29d51055478a38b8f6bc9
Dan Hope <dan.g.hope@gmail.com>
2017-04-06 17:25:16 -0400

    Added proper logic to dealer

--- src/randgame.py
+++ src/randgame.py
@@ -7,9 +7,15 @@ import argparse
 
 def handValue(hand):
     val = 0
+    soft = 0
     for i in hand:
+        if(i.number == "ace"):
+            soft += 1
         val += i.value
-    return val
+        if(val > 21 and soft > 0):
+            val -= 10
+            soft -= 1
+    return val, soft
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='BlackJack Neural Network Tester')
@@ -32,52 +38,77 @@ if __name__ == "__main__":
 
         dealer = []
 
-        # starting hand
+        # Deal cards to dealer and player
         hand = []
         hand.append(deck.draw())
+        dealer.append(deck.draw())
         hand.append(deck.draw())
+        dealer.append(deck.draw())
 
         #print("Starting hand %s %s" %(hand[0],hand[1]))
         j = 0
+        dealer_val = -1
+        passed = False
         while j < max_steps:
             j += 1
 
-            # roll for random movement
-            a = random.choice(range(actionCount))
+            val, soft = handValue(hand)
 
-            passed = False
-            # get new state and reward
-            if a == 0: # HIT
-                card = deck.draw()
-                hand.append(card)
-            elif a == 1: # PASS
+            if val == 21:
+                # You got blackjack
+                r = 1
                 passed = True
+        
+            if(not passed):
+                # roll for random movement
+                a = random.choice(range(actionCount))
 
-            if handValue(hand) == 21:
-                # larger reward for getting 21 before dealer
-                r = 2
-            elif handValue(hand) > 21:
-                # larger penalty for busting
-                r = -2
-            else:
-                r = 0
+                # get new state and reward
+                if a == 0: # HIT
+                    card = deck.draw()
+                    hand.append(card)
+                elif a == 1: # PASS
+                    passed = True
 
+                val, _ = handValue(hand)
 
+                if val > 21:
+                    r = -1
+                    passed = False
+                else:
+                    r = 0
+
+            
             # perform dealer logic
             if passed:
-                dealer.append(deck.draw())
-                dealer.append(deck.draw())
-                while handValue(dealer) < handValue(hand):
+                # Determine value of dealers current hand
+                dealer_val, dealer_soft = handValue(dealer)
+
+                # If dealer has blackjack everyone losses
+                if(dealer_val == 21):
+                    r = -1
+
+                # Hit on soft 17 stand elsewise
+                while ((dealer_val < 17) or (dealer_val == 17 and dealer_soft > 0)):   
                     dealer.append(deck.draw())
-                if handValue(dealer) > 21:
+                    dealer_val, dealer_soft = handValue(dealer)
+
+                if (dealer_val > 21) or (val > dealer_val):
                     # dealer bust, player wins
                     r = 1
+                elif (dealer_val > val):
+                    r = -1
                 else:
-                    r = -1 # dealer beat player
+                    r = 0.5
+                    
 
             if r != 0:
                 if r > 0:
                     wins += 1
+                    # print("Win > D: %d, P: %d" %(dealer_val,val))
+                else:
+                    if(dealer_val > 21):
+                        print("Loss > D: %d, P: %d" %(dealer_val,val))
                 break
 
     print("Win percentage\n%f" %(wins/float(num_runs)))
